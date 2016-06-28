7ccfa5d868aa899be6e446e27c827563b3e38183
leesei <leesei@gmail.com>
2016-06-28 13:05:52 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+Clone this repo and serve it via HTTP server (e.g. `ecstatic`, `python -m SimpleHTTPServer`).  
+Visit the page and enter the following snippets in console.
+
+These are exposed to `window`:
+- viewer
+- viewport
+- tiledImage
+
+### Fail case
+
+```js
+viewport.zoomTo(tiledImage.imageToViewportZoom(2.0))
+// wait for 'view-updated'
+viewport.setRotation(0)
+// wait for 'view-updated'
+viewport.setRotation(44)
+```
+
+The tiles do not fill canvas after these operations (the white canvas background  can be seen).
+
+Resizing the window will fix the issue, and subsequent calls to rotation will not trigger the issue.
+
+OSD 2.1.0 does not have this issue (change the `<script>` in `index.html`).
+
+### Success case
+
+Single paste and execute, without waiting for `view-updated`
+
+```js
+viewport.zoomTo(tiledImage.imageToViewportZoom(2.0))
+viewport.setRotation(0)
+viewport.setRotation(44)
+```
+
+### Success case
+
+Without `setRotation(0)`
+
+```js
+viewport.zoomTo(tiledImage.imageToViewportZoom(2.0))
+// wait for 'view-updated'
+viewport.setRotation(44)
+```
---
+++ script.js
@@ -0,0 +1,78 @@
+var VIEWPORT_ZOOM_MIN = 0.9;
+var IMAGE_ZOOM_MAX = 2;
+
+var tileSource = {
+  Image: {
+    xmlns: "http://schemas.microsoft.com/deepzoom/2008",
+    Url: "http://openseadragon.github.io/example-images/highsmith/highsmith_files/",
+    Format: "jpg",
+    Overlap: "2",
+    TileSize: "256",
+    Size: {
+      Height: "9221",
+      Width: "7026"
+    }
+  }
+};
+
+window.viewer = OpenSeadragon({
+  id: "openseadragon",
+  tabIndex: '',
+  animationTime: 0.5,
+  blendTime: 0.1,
+  debugMode: true,
+  immediateRender: true,
+  showNavigator: true,
+  navigatorSizeRatio: 0.2,
+  navigatorAutoFade: false,
+  showNavigationControl: false,
+  autoResize: true,
+  constrainDuringPan: true,
+  defaultZoomLevel: 1,
+  minZoomImageRatio: VIEWPORT_ZOOM_MIN,
+  maxZoomPixelRatio: IMAGE_ZOOM_MAX,
+  gestureSettingsMouse: {
+    clickToZoom: false,
+    dblClickToZoom: false
+  },
+  gestureSettingsTouch: {
+    clickToZoom: false,
+    dblClickToZoom: false
+  },
+  showNavigationControl: false,
+  showNavigator: true
+});
+
+viewer.addHandler('open', function () {
+  console.log('opened');
+  window.viewport = viewer.viewport;
+  window.tiledImage = viewer.world.getItemAt(0)
+  window.navigator = viewer.navigator;
+});
+
+function onViewChanged () {
+  var viewport = viewer.viewport;
+  var boundRect = viewport.viewportToImageRectangle(viewport.getBounds());
+  var viewInfo = {
+    // don't expose OpenSeadragon.Rect
+    boundRect: {
+      x: boundRect.x,
+      y: boundRect.y,
+      width: boundRect.width,
+      height: boundRect.height
+    },
+    center: {
+      x: (boundRect.x + boundRect.width) / 2,
+      y: (boundRect.y + boundRect.height) / 2
+    },
+    imageZoom: viewport.viewportToImageZoom(viewport.getZoom()),
+    rotation: viewport.getRotation()
+  };
+  console.log('view-updated', viewInfo);
+}
+
+viewer.addHandler('rotate', onViewChanged);
+viewer.addHandler('animation-finish', onViewChanged);
+viewer.addHandler('resize', onViewChanged);
+
+viewer.open(tileSource);
